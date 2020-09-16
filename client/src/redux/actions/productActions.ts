06070331d8dd7ff42617ff4afe4dc45e8567dfdf
Tugay Cem Evci <46964249+tugayevci@users.noreply.github.com>
2020-09-16 17:27:14 +0300

    price filter added

--- client/src/redux/actions/productActions.ts
+++ client/src/redux/actions/productActions.ts
@@ -15,26 +15,28 @@ export const getProducts = async (filters: any[]): Promise<IGetProductsAction> =
   let filteredProducts: Product[] = []
 
   temp.forEach((item) => {
-    console.log('item', item)
-
     let result = true
 
     filters.forEach((filter) => {
-      console.log('filter', filter)
+      if (filter.key !== 'price') {
+        let findResult = item.filters.find((itemFilter) => itemFilter.key === filter.key && filter.values.includes(itemFilter.name))
 
-      let findResult = item.filters.find((itemFilter) => itemFilter.key === filter.key && filter.values.includes(itemFilter.name))
-      console.log('findResult', findResult)
+        if (!findResult) result = false
+      } else {
+        const values: number[] = filter.values
+          .map((x: any) => x.split('-'))
+          .flat(2)
+          .map((price: string) => parseFloat(price))
+        const maxValue = Math.max(...values)
+        const minValue = Math.min(...values)
 
-      if (!findResult) result = false
-
-      console.log('result', result)
+        if (item.price > maxValue || item.price < minValue) result = false
+      }
     })
 
     if (result) {
       filteredProducts.push(item)
     }
-
-    console.log('final result', result)
   })
 
   return {
